da205024ced81112d7b1648bffa7e81314aa1e4f
Alex Kott <tetlokax@gmail.com>
2016-07-14 14:10:21 +0200

    feat(posts): display post list on startpage

--- routes/pages/index.js
+++ routes/pages/index.js
@@ -1,6 +1,14 @@
 const router = require('express').Router(); // eslint-disable-line
+const Post = require(`${__base}/models/post`);
 
 router.route('/')
-    .get((req, res) => res.render('index', {}));
+    .get((req, res) => {
+        Post.find({}, (err, data) => {
+            if (err) {
+                return res.status(500).send({ errors: [{ detail: err }] });
+            }
+            return res.render('posts', { posts: data });
+        });
+    });
 
 module.exports = router;
